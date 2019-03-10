aad226e081f1b86b891be8a5c37d15e36be80018
huanshenyi <txy1226052@gmail.com>
2019-03-10 14:15:31 +0900

    search finish

--- src/pages/city/components/Search.vue
+++ src/pages/city/components/Search.vue
@@ -1,12 +1,75 @@
 <template>
-  <div class="search">
-    <input class="search-input" type="text" placeholder="都市名を入力してください" />
+  <div>
+    <div class="search">
+      <input class="search-input" type="text"
+             v-model="keyword"
+             placeholder="都市名を入力してください"
+      />
+    </div>
+    <div
+      class="search-content"
+      ref="search"
+      v-show="keyword"
+    >
+      <ul>
+        <li class="search-item border-bottom"
+            v-for="item in list"
+            :key="item.id"
+        >
+          {{item.name}}
+        </li>
+        <li class="search-item border-bottom" v-show="hasNoDate">
+          マッチングした内容存在しません
+        </li>
+      </ul>
+    </div>
   </div>
 </template>
 
 <script>
+import Bscroll from 'better-scroll'
 export default {
-  name: 'CitySearch'
+  name: 'CitySearch',
+  props: {
+    cities: Object
+  },
+  data: function () {
+    return {
+      keyword: '',
+      list: [],
+      timer: null
+    }
+  },
+  computed: {
+    hasNoDate: function () {
+      return !this.list.length
+    }
+  },
+  watch: {
+    keyword: function () {
+      if (this.timer) {
+        clearTimeout(this.timer)
+      }
+      if (!this.keyword) {
+        this.list = []
+        return
+      }
+      this.timer = setTimeout(() => {
+        const result = []
+        for (let i in this.cities) {
+          this.cities[i].forEach((value) => {
+            if (value.spell.indexOf(this.keyword) > -1 || value.name.indexOf(this.keyword) > -1) {
+              result.push(value)
+            }
+          })
+        }
+        this.list = result
+      }, 100)
+    }
+  },
+  mounted () {
+    this.scroll = new Bscroll(this.$refs.search)
+  }
 }
 </script>
 
@@ -25,4 +88,18 @@ export default {
      text-align center
      border-radius .06rem
      color #666
+   .search-content
+     z-index 1
+     overflow hidden
+     position :absolute
+     top: 1.58rem
+     left 0
+     right 0
+     bottom 0
+     background #eee
+     .search-item
+      line-height .62rem
+      padding-left .2rem
+      background #fff
+      color :#666
 </style>
