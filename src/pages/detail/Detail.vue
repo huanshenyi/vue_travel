c64c4ef2437fd1030830520189127d67191f8037
huanshenyi <txy1226052@gmail.com>
2019-04-13 20:05:32 +0900

    detail-list

--- src/pages/detail/Detail.vue
+++ src/pages/detail/Detail.vue
@@ -2,18 +2,43 @@
   <div>
     <detail-banner></detail-banner>
     <detail-header></detail-header>
-    <div class="content"></div>
+    <div class="content">
+      <detail-list :list="list"></detail-list>
+    </div>
   </div>
 </template>
 
 <script>
 import DetailBanner from './components/Banner'
 import DetailHeader from './components/Header'
+import DetailList from './components/List'
 export default {
   name: 'Detail',
   components: {
     DetailBanner,
-    DetailHeader
+    DetailHeader,
+    DetailList
+  },
+  data: function () {
+    return {
+      list: [{
+        title: '大人',
+        children: [{
+          title: '大人三人グループ',
+          children: [{
+            title: '大人三人グループ-指定店舗'
+          }]
+        }, {
+          title: '大人五人グループ'
+        }]
+      }, {
+        title: '学生'
+      }, {
+        title: '子供'
+      }, {
+        title: 'サービス'
+      }]
+    }
   }
 }
 </script>
