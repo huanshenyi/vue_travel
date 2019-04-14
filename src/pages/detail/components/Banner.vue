e85e16ec110bbdc0309221b2d516899fb5df4570
huanshenyi <txy1226052@gmail.com>
2019-04-14 15:39:51 +0900

    animation finished

--- src/pages/detail/components/Banner.vue
+++ src/pages/detail/components/Banner.vue
@@ -13,15 +13,19 @@
             </div>
           </div>
       </div>
-      <common-gallary
-        :imgs="bannerImgs"
-        v-show="showGallary"
-        @close="handleGallaryClose"></common-gallary>
+      <fade-animation>
+        <common-gallary
+          :imgs="bannerImgs"
+          v-show="showGallary"
+          @close="handleGallaryClose"
+        ></common-gallary>
+      </fade-animation>
     </div>
 </template>
 
 <script>
 import CommonGallary from 'common/gallary/Gallary'
+import FadeAnimation from 'common/fade/FadeAnimation'
 export default {
   name: 'DetailBanner',
   props: {
@@ -29,7 +33,7 @@ export default {
     bannerImg: String,
     bannerImgs: Array
   },
-  components: {CommonGallary},
+  components: {CommonGallary, FadeAnimation},
   comments: {
     CommonGallary
   },
